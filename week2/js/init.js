d41eb2b43abe6937b41852092fed91e954cfcbd7
Trinh Le <ltqa1202@g.ucla.edu>
2023-04-17 14:10:54 -0700

    finished week2 assignment

--- week2/js/init.js
+++ week2/js/init.js
@@ -4,7 +4,7 @@
 
 // // use the variables
 // const map = L.map('the_map').setView(mapCenter, zoomLevel);
-let mapOptions = { 'center': [34.0709, -118.444], 'zoom': 5 }
+let mapOptions = { 'center': [36.2048, 138.2529], 'zoom': 6 }
 
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -20,6 +20,8 @@ function addMarker(lat, lng, title, message) {
     return message
 }
 
+
+
 // use our marker functions
 // addMarker(37, -122, 'home', 'home land!')
 // addMarker(32, -118, 'work', 'where i work land!')
@@ -30,6 +32,50 @@ fetch("map.geojson") // fetch the GeoJSON file, this is the name from step 2.
         return response.json();
     })
     .then(data => {
+        var takagiIcon = L.icon({
+            iconUrl: data.features[0].properties.imageURL,
+            iconSize: [38, 38] // size of the icon
+        });
+        var demonSlayerIcon = L.icon({
+            iconUrl: data.features[1].properties.imageURL,
+            iconSize: [38, 38] // size of the icon
+
+        });
+        var yourNameIcon = L.icon({
+            iconUrl: data.features[2].properties.imageURL,
+            iconSize: [38, 38] // size of the icon
+        });
         // Basic Leaflet method to add GeoJSON data
-        L.geoJSON(data).addTo(map)
+        L.geoJSON(data, {
+            pointToLayer: function(feature, latlng) {
+                //     const icons = new L.Icon({
+                //         iconUrl: feature,
+                //         iconSize: [40, 40],
+                //     });
+                //     return L.marker(latlng, { icon: icons })
+                // }
+                if (feature.properties.place === "Takagi-san") {
+                    return L.marker(latlng, { icon: takagiIcon });
+                } else if (feature.properties.place === "Demon Slayer") {
+                    return L.marker(latlng, { icon: demonSlayerIcon });
+                } else if (feature.properties.place === "Your Name") {
+                    return L.marker(latlng, { icon: yourNameIcon });
+                }
+            },
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup(feature.properties.details);
+            }
+        }).addTo(map)
     });
+
+/*
+    L.geoJSON(geojson, {
+    pointToLayer: function (feature, latlng) {
+        const icons = new L.Icon({
+            iconUrl: "https://i.imgur.com/ZcGeIVz.png",
+            iconSize: [40, 40],
+        });
+        return L.marker(latlng, {icon: icons});
+    }
+}).addTo(mymap);
+    */
