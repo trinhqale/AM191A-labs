4e958be238a65d2879e4a429d79494a0a24c87bd
Trinh <ltqa1202@g.ucla.edu>
2023-04-27 16:07:51 -0700

    finsihed lab3

--- week3/js/init.js
+++ week3/js/init.js
@@ -32,18 +32,19 @@ fetch("map.geojson") // fetch the GeoJSON file, this is the name from step 2.
         return response.json();
     })
     .then(data => {
+        var button 
         var takagiIcon = L.icon({
             iconUrl: data.features[0].properties.imageURL,
-            iconSize: [38, 38] // size of the icon
+            iconSize: [40, 40] // size of the icon
         });
         var demonSlayerIcon = L.icon({
             iconUrl: data.features[1].properties.imageURL,
-            iconSize: [38, 38] // size of the icon
+            iconSize: [40, 40] // size of the icon
 
         });
         var yourNameIcon = L.icon({
             iconUrl: data.features[2].properties.imageURL,
-            iconSize: [38, 38] // size of the icon
+            iconSize: [40, 40] // size of the icon
         });
         // Basic Leaflet method to add GeoJSON data
         L.geoJSON(data, {
@@ -64,8 +65,57 @@ fetch("map.geojson") // fetch the GeoJSON file, this is the name from step 2.
             },
             onEachFeature: function(feature, layer) {
                 layer.bindPopup(feature.properties.details);
+            },
+            
+        }).addTo(map);
+        var i = 0;    
+        var myButton = L.control({ position: 'topright' });
+        myButton.onAdd = function(map) {
+          var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+          var button = L.DomUtil.create('a', 'leaflet-control-button', container);
+          button.innerHTML = 'Next';
+
+          L.DomEvent.on(button, 'click', function() {
+            next_location = data.features[i].geometry.coordinates  
+            console.log(next_location)
+            // call a function to zoom to a specific location
+            map.flyTo([next_location[1], next_location[0]], 12);
+            i+=1;
+            if(i == data.features.length){
+                i = 0;
+                console.log(i)
             }
-        }).addTo(map)
+            
+            
+          });
+          return container;
+        };
+        myButton.addTo(map);
+
+       // create a custom control for the reset zoom button
+var resetZoomControl = L.Control.extend({
+    options: {
+      position: 'topright'
+    },
+  
+    onAdd: function (map) {
+      // create the button element
+      var button = L.DomUtil.create('button', 'reset-zoom-button');
+      button.innerHTML = 'Reset Zoom';
+  
+      // add click event listener to the button
+      L.DomEvent.addListener(button, 'click', function () {
+        map.flyTo(mapOptions.center, mapOptions.zoom);
+      });
+  
+      return button;
+    }
+  });
+  
+  // add the custom control to the map
+  map.addControl(new resetZoomControl());
+  
+        
     });
 
 /*
